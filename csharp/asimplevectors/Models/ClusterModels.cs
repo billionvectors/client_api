7b8f7801a9ba86544b6245da2ad1bdaac885ac5b
Kim Jungho <j21chon@gmail.com>
2025-02-03 09:37:05 +0900

    update configuration

--- csharp/asimplevectors/Models/ClusterModels.cs
+++ csharp/asimplevectors/Models/ClusterModels.cs
@@ -44,19 +44,47 @@ namespace asimplevectors.Models
 
     public class Node
     {
+        [JsonPropertyName("rpc_addr")]
         public string RpcAddr { get; set; }
+        [JsonPropertyName("api_addr")]
         public string ApiAddr { get; set; }
     }
 
     public class ClusterMetricsResponse
     {
-        public string State { get; set; }
+        [JsonPropertyName("running_state")]
+        public RunningState RunningState { get; set; }
         public int Id { get; set; }
+        [JsonPropertyName("current_term")]
         public int CurrentTerm { get; set; }
         public ClusterVote Vote { get; set; }
+        [JsonPropertyName("last_log_index")]
         public int? LastLogIndex { get; set; }
-        public int? LastQuorumAcked { get; set; }
+        [JsonPropertyName("last_applied")]
+        public LogId LastApplied { get; set; }
+        public object Snapshot { get; set; }
+        public object Purged { get; set; }
+        public string State { get; set; }
+        [JsonPropertyName("current_leader")]
+        public int CurrentLeader { get; set; }
+        [JsonPropertyName("millis_since_quorum_ack")]
+        public long MillisSinceQuorumAck { get; set; }
+        [JsonPropertyName("last_quorum_acked")]
+        public long LastQuorumAcked { get; set; }
+        [JsonPropertyName("membership_config")]
         public MembershipConfig MembershipConfig { get; set; }
         public Dictionary<int, long> Heartbeat { get; set; }
+        public Dictionary<int, LogId> Replication { get; set; }
+
+        public override string ToString()
+        {
+            return $"State: {State}, NodeCount: {MembershipConfig?.Membership?.Nodes?.Count ?? 0}, " +
+                   $"IndexCount: {LastLogIndex ?? 0}, ReplicationCount: {Replication?.Count ?? 0}";
+        }
+    }
+
+    public class RunningState
+    {
+        public string Ok { get; set; }
     }
 }
